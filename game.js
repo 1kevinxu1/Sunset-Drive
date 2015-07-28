88e2aca8ce5f0c58ed35929656b27453fb9aaa70
Kevin <1kevinxu1@gmail.com>
2015-07-28 13:56:38 -0700

    first refactored version of MPV

--- game.js
+++ game.js
@@ -1,18 +1,94 @@
 (function () {
   // Make a namespace `Assessment`.
-  if(typeof Game === "undefined") {
-    window.Util = {};
+  if(typeof Racer === "undefined") {
+    window.Racer = {};
   };
 
-  Game.width         = canvasEl.width;
-  Game.height        = canvasEl.height;
-  Game.cameraHeight  = 50;
-  Game.cameraDepth   = 15;
-  Game.playerX       = 0;
-  Game.playerZ       = 0;
-  Game.roadWidth     = 60;
-  Game.segments      = [];
-  Game.speed         = 10;
-  Game.acceleration  = .2;
+ var Game = Racer.Game = function(context, xDim, yDim) {
+    this.ctx = context;
+    this.width = xDim;
+    this.height = yDim;
+    this.cameraHeight  = 50;
+    this.cameraDepth   = 15;
+    this.playerX       = 0;
+    this.playerZ       = 0;
+    this.roadWidth     = 60;
+    this.segments      = [];
+    this.speed         = 10;
+    this.acceleration  = -.5;
+    this.maxSpeed      = 120;
+    this.dx            = 0;
+    this.segments      = [];
+    for(var n = 0 ; n < 500 ; n++) {
+      var color =  Math.floor(n)%2 ? '#696969' : 'white';
+      this.segments.push([n*200, (n+1)*200, color]);
+    };
+  };
 
+  Game.KEYS = {
+    LEFT:  37,
+    UP:    38,
+    RIGHT: 39,
+    DOWN:  40,
+    A:     65,
+    D:     68,
+    S:     83,
+    W:     87
+  };
+
+  $(document).on('keydown', Game.keyPressed);
+  $(document).on('keyup',  Game.keyUnpressed);
+
+  Game.keyPressed = function(event) {
+    if (event.which === Game.KEYS.UP || event.which === Game.KEYS.W) {
+      this.acceleration = 2;
+    };
+    if (event.which === Game.KEYS.RIGHT || event.which === Game.KEYS.D) {
+      console.log("DOWN");
+      this.dx = 1/55;
+    };
+    if (event.which === Game.KEYS.LEFT || event.which === Game.KEYS.A) {
+      this.dx = -1/55;
+    };
+  };
+
+  Game.keyUnpressed = function(event) {
+    if (event.which === Game.KEYS.UP || event.which === Game.KEYS) {
+      this.acceleration = -.5;
+    };
+    if (event.which === Game.KEYS.RIGHT) {
+      if (dx === 1/55) {
+        this.dx = 0;
+      }
+    };
+    if (event.which === Game.KEYS.LEFT) {
+      if (dx === -1/55) {
+        this.dx = 0;
+      }
+    };
+  };
+
+  Game.prototype.render = function() {
+    if (this.acceleration < 0) {
+      this.speed += this.speed <= 0? 0 : this.acceleration;
+    } else {
+      this.speed += this.speed >= this.maxSpeed? 0 : this.acceleration;
+    }
+    this.playerX += this.speed*this.dx;
+    // this.ctx.clearRect(0, 0, this.width, this.height);
+    this.playerZ += this.speed;
+
+    var currentSegment = Math.floor(this.playerZ/200) % this.segments.length;
+    for(i = 0 ; i < 20 ; i++) {
+      n = i + currentSegment;
+      if (this.segments[n][0] > this.playerZ) {
+        debugger;
+        Util.drawSegment(this, this.segments[n][0], this.segments[n][1], this.segments[n][2]);
+      }
+    }
+  };
+
+  Game.prototype.run = function() {
+    setInterval(this.render.bind(this), 1000/60)
+  };
 })();
--- util.js
+++ util.js
@@ -1,10 +1,22 @@
 (function () {
-  // Make a namespace `Assessment`.
   if(typeof Util === "undefined") {
     window.Util = {};
   };
 
-  Util.prototype.drawPolygon = function(ctx,x1,y1,x2,y2,x3,y3,x4,y4,color) {
+  Util.project = function(game, worldX, worldY, worldZ) {
+    debugger;
+    var cameraX    = worldX - game.playerX;
+    //camera height is essentially 'playerY'
+    var cameraY    = worldY - game.cameraHeight;
+    var cameraZ    = worldZ - game.playerZ;
+    var scale      = game.cameraDepth/cameraZ;
+    var x          = Math.round((game.width/2)  + (scale * cameraX  * game.width/2));
+    var y          = Math.round((game.height/2) - (scale * cameraY  * game.height/2));
+    var w          = Math.round((scale * game.roadWidth * game.width/2));
+    return [x, y, w]
+  };
+
+  Util.polygon = function(ctx, x1, y1, x2, y2, x3, y3, x4, y4, color) {
     ctx.fillStyle = color;
     ctx.beginPath();
     ctx.moveTo(x1, y1);
@@ -13,11 +25,11 @@
     ctx.lineTo(x4, y4);
     ctx.closePath();
     ctx.fill();
-  );
+  };
 
-  Util.prototype.drawSegment(start, end, color)) {
-    var start = project(0, 0, start);
-    var end   = project(0, 0, end);
+  Util.drawSegment = function(game, start, end, color) {
+    var start = Util.project(game, 0, 0, start);
+    var end   = Util.project(game, 0, 0, end);
 
     var x1 = start[0];
     var y1 = start[1];
@@ -25,32 +37,7 @@
     var x2 = end[0];
     var y2 = end[1];
     var w2 = end[2];
-    polygon(context, x1-w1, y1, x1+w1, y1, x2+w2, y2, x2-w2, y2, color);
-
-  };
-
-  Util.prototype.project = function(playerX,
-                                    cameraHeight,
-                                    playerZ,
-                                    worldX,
-                                    worldY,
-                                    worldZ,
-                                    width,
-                                    height,
-                                    roadWidth
-                                   ) {
-    var cameraX    = worldX - playerX;
-    //camera height is essentially 'playerY'
-    var cameraY    = worldY - cameraHeight;
-    var cameraZ    = worldZ - playerZ;
-    var scale      = cameraDepth/cameraZ;
-    var x          = Math.round((width/2)  + (scale * cameraX  * width/2));
-    var y          = Math.round((height/2) - (scale * cameraY  * height/2));
-    var w          = Math.round((scale * roadWidth * width/2));
-    return [x, y, w]
+    debugger;
+    Util.polygon(game.ctx, x1-w1, y1, x1+w1, y1, x2+w2, y2, x2-w2, y2, color);
   };
 })();
-
-// var newGame = new Asteroids.Game(canvasEl.width, canvasEl.height);
-// var newView = new Asteroids.GameView(ctx, newGame);
-// newView.start();
